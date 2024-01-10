20c5ab41a10ddffd636d46e113a69476d8330db4
syedbilalWW <syedbilalhaider1998@gmail.com>
2024-01-10 15:56:43 +0500

    Custom UI

--- src/views/campaigns/CampaignsView.tsx
+++ src/views/campaigns/CampaignsView.tsx
@@ -44,33 +44,30 @@ function CampaignsView({ ttclid, gclid, kwaiid }: Props) {
     //   {isSuccess ? (
     //     <SuccessScreen />
     //   ) : (
-    <HStack
-      w={"90%"}
-      maxW={"1300px"}
-      mx={"auto"}
-      gap={14}
-      justify={"center"}
-      align={"start"}
-      paddingBottom={10}
-      paddingY={{ base: 4, lg: 8 }}
-    >
-      <Flex
-        direction={"column"}
-        maxW={600}
-        align={"center"}
-        gap={{ base: 5, lg: 10 }}
-      >
-        <Box
-          as={Image}
-          loading="eager"
-          h={"60px"}
-          w={"370px"}
+    <div style={{ width: '90%', maxWidth: '1300px', margin: '0 auto 0 auto', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: 10, flexDirection: 'row' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', maxWidth: 600, alignItems: 'center' }}>
+        <Image
           src={CampaginBanner2}
+          loading="lazy"
+          height={60}
+          width={370}
           alt="Top Banner"
-          borderRadius={10}
+          style={{ borderRadius: 10 }}
         />
         <PhoneOtpScreen handleSuccess={handleSuccess} />
-      </Flex>
+      </div>
+      {/* <div>
+          <Image
+            src={WWBanner}
+            loading="lazy"
+            // height={60}
+            width={370}
+            // display={{ base: "none", lg: "block" }}
+            // width={"50%"}
+            alt="Side Banner"
+            style={{height:'100%', backgroundColor:'red'}}
+          />
+        </div> */}
       <Box
         as={Image}
         position={"sticky"}
@@ -81,7 +78,46 @@ function CampaignsView({ ttclid, gclid, kwaiid }: Props) {
         loading={"lazy"}
         alt="Side Banner"
       />
-    </HStack>
+
+    </div>
+    // <HStack
+    //   w={"90%"}
+    //   maxW={"1300px"}
+    //   mx={"auto"}
+    //   gap={14}
+    //   justify={"center"}
+    //   align={"start"}
+    //   paddingBottom={10}
+    //   paddingY={{ base: 4, lg: 8 }}
+    // >
+    //   <Flex
+    //     direction={"column"}
+    //     maxW={600}
+    //     align={"center"}
+    //     gap={{ base: 5, lg: 10 }}
+    //   >
+    //     <Box
+    //       as={Image}
+    //       loading="eager"
+    //       h={"60px"}
+    //       w={"370px"}
+    //       src={CampaginBanner2}
+    //       alt="Top Banner"
+    //       borderRadius={10}
+    //     />
+    //     <PhoneOtpScreen handleSuccess={handleSuccess} />
+    //   </Flex>
+    //   <Box
+    //     as={Image}
+    //     position={"sticky"}
+    //     top={120}
+    //     src={WWBanner}
+    //     display={{ base: "none", lg: "block" }}
+    //     width={"50%"}
+    //     loading={"lazy"}
+    //     alt="Side Banner"
+    //   />
+    // </HStack>
     //   )}
     // </>
   );

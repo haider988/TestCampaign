fbdeba9971936d09cc8d8210af3aa87afcdd0b2e
syedbilalWW <syedbilalhaider1998@gmail.com>
2024-01-10 10:56:27 +0500

    Adding SpeedInsight Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { AppProps } from "next/app";
 import theme from "../theme.fonts";
 import { CampaignProvider } from "../src/context/CampaignProvider";
@@ -7,6 +8,7 @@ import { fonts } from "../src/fonts/poppins";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <SpeedInsights/>
       <style jsx global>
         {`
           :root {
